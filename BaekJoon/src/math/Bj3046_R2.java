85a9e159a35875c62a70e09e0e512de7e970f671
minzzun99 <skg06123@naver.com>
2024-05-19 22:29:14 +0900

    05.19 백준

--- BaekJoon/src/math/Bj3046_R2.java
+++ BaekJoon/src/math/Bj3046_R2.java
@@ -1,18 +1,17 @@
+package math;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-public class Bj10757 {
+public class Bj3046_R2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int R1 = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
 
-        BigInteger A = new BigInteger(st.nextToken());
-        BigInteger B = new BigInteger(st.nextToken());
-
-        A = A.add(B);
-        System.out.println(A);
+        System.out.println((S*2) - R1);
     }
 }
