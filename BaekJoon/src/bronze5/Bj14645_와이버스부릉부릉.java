63d3d495781a93849fe0d3ed9e20cad95a3cd842
minzzun99 <skg06123@naver.com>
2024-06-08 15:20:55 +0900

    06.08 백준

--- BaekJoon/src/bronze5/Bj14645_와이버스부릉부릉.java
+++ BaekJoon/src/bronze5/Bj14645_와이버스부릉부릉.java
@@ -1,7 +1,18 @@
 package bronze5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Bj14645_와이버스부릉부릉 {
-    public static void main(String[] args) {
-        System.out.println("I'm Sexy");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            br.readLine();
+        }
+        System.out.println("비와이");
     }
 }
