a179309906885f733a668d68c3e6763d82d1dcca
minzzun99 <skg06123@naver.com>
2024-06-29 19:48:48 +0900

    06.29 백준

---
+++ BaekJoon/src/bronze5/Bj10189_HOOK.java
@@ -0,0 +1,10 @@
+package bronze5;
+
+public class Bj10189_HOOK {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
