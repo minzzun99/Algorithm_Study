a03511d4b8fca25fa38dd6247d1569e3c714ce81
minzzun99 <skg06123@naver.com>
2024-09-30 18:39:44 +0900

    09.30_백준_Silver5_정열적인 정렬

---
+++ BaekJoon/src/sort/Bj16212_정열적인정렬.java
@@ -0,0 +1,26 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bj16212_정열적인정렬 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(' ');
+        }
+        System.out.println(sb);
+    }
+}
