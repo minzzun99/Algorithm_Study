e9c968b70f2afe044c3888920e6963df624c1503
minzzun99 <skg06123@naver.com>
2024-10-14 16:22:38 +0900

    10.14_백준_Gold4_도시 분할 계획

---
+++ BaekJoon/src/tree/Bj1647_도시분할계획.java
@@ -0,0 +1,74 @@
+package tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Bj1647_도시분할계획 {
+    private static int N, M;
+    private static List<Node>[] adjList;
+    private static boolean[] visited;
+
+    private static class Node {
+        int next;
+        int cost;
+
+        Node(int next, int cost) {
+            this.next = next;
+            this.cost = cost;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        adjList = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            adjList[start].add(new Node(end, cost));
+            adjList[end].add(new Node(start, cost));
+        }
+
+        System.out.println(countWeight(1));
+    }
+
+    private static int countWeight(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
+            return Integer.compare(o1.cost, o2.cost);
+        });
+        int max = 0;
+        int count = 0;
+        pq.add(new Node(start, 0));
+
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+            if (visited[now.next]) {
+                continue;
+            }
+            max = Math.max(max, now.cost);
+            count += now.cost;
+            visited[now.next] = true;
+
+            for (Node next : adjList[now.next]) {
+                if (!visited[next.next]) {
+                    pq.add(next);
+                }
+            }
+        }
+        return count - max;
+    }
+}
