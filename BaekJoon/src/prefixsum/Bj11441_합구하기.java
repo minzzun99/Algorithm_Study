7689f887f95d7871af132a2ff4e9d36b728f19da
minzzun99 <skg06123@naver.com>
2024-10-20 16:58:55 +0900

    10.20_백준_Silver3_합 구하기

---
+++ BaekJoon/src/prefixsum/Bj11441_합구하기.java
@@ -0,0 +1,30 @@
+package prefixsum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj11441_합구하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()) + arr[i -1];
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            sb.append(arr[end] - arr[start - 1]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
