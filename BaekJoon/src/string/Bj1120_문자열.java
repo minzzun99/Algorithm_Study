12591701183305f6a80400971c5bb6e24ebc22e1
minzzun99 <skg06123@naver.com>
2024-10-28 13:22:23 +0900

    10.28_백준_Silver4_문자열

---
+++ BaekJoon/src/string/Bj1120_문자열.java
@@ -0,0 +1,29 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj1120_문자열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String A = st.nextToken();
+        String B = st.nextToken();
+
+        int result = A.length();
+        for (int i = 0; i < B.length() - A.length() + 1; i++) {
+            int temp = 0;
+            for (int j = 0; j < A.length(); j++) {
+                if (A.charAt(j) != B.charAt(j + i)) {
+                    temp++;
+                }
+            }
+
+            result = Math.min(result, temp);
+        }
+
+        System.out.println(result);
+    }
+}
