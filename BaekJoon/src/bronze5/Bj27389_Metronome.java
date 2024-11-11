814a1e89a1499afceaab1510bf61d882d3bf96cc
minzzun99 <skg06123@naver.com>
2024-11-11 13:30:50 +0900

    11.11_백준_Bronze5_Metronome

---
+++ BaekJoon/src/bronze5/Bj27389_Metronome.java
@@ -0,0 +1,13 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj27389_Metronome {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.printf("%.2f", N / 4.0);
+    }
+}
