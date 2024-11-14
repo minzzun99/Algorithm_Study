8feb04d69c60ae0f1e2a8aaa7003f019b211e70f
minzzun99 <skg06123@naver.com>
2024-11-14 15:16:57 +0900

    11.14_백준_Bronze5_Gum Gum for Jay Jay

---
+++ BaekJoon/src/bronze5/Bj26489_GumGumForJayJay.java
@@ -0,0 +1,24 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj26489_GumGumForJayJay {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+
+        while (true) {
+            String s = br.readLine();
+            if (s == null) {
+                break;
+            }
+
+            if (s.equals("gum gum for jay jay")) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
