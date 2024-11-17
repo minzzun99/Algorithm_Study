a517e355fe9881d65380f493bbfde5a3f7df20b3
minzzun99 <skg06123@naver.com>
2024-11-17 13:44:23 +0900

    24.11.17 SWEA 1220_Magnetic (수정)

--- SWExpertAcademy/src/ssafy13/SWEA1220_Magnetic.java
+++ SWExpertAcademy/src/ssafy13/SWEA1220_Magnetic.java
@@ -35,17 +35,13 @@ public class SWEA1220_Magnetic {
     private static int countDeadLock() {
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
+            boolean flag = false;
             for (int j = 0; j < arr.length; j++) {
-                if (arr[j][i] == 0 || arr[j][i] == 2) {
-                    continue;
-                }
-
-                for (int k = j + 1; k < arr.length; k++) {
-                    j = k;
-                    if (arr[k][i] == 2) {
-                        count++;
-                        break;
-                    }
+                if (arr[j][i] == 1) {
+                    flag = true;
+                } else if (arr[j][i] == 2 && flag) {
+                    count++;
+                    flag = false;
                 }
             }
         }
