5422eb6e3cfdfc0bbc7362bfe53936f44eb88e7a
minzzun99 <skg06123@naver.com>
2024-11-19 16:54:43 +0900

    11.19_백준_Bronze5_Hour

---
+++ BaekJoon/src/bronze5/Bj27327_Hour.java
@@ -0,0 +1,13 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj27327_Hour {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N * 24);
+    }
+}
