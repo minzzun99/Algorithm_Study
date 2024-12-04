a9956abefecd86e75c15a41d9ff566361b2e00e3
minzzun99 <skg06123@naver.com>
2024-12-04 14:55:55 +0900

    12.04_백준_Bronze5_Intercepting Information

---
+++ BaekJoon/src/bronze5/Bj26209_InterceptingInformation.java
@@ -0,0 +1,17 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj26209_InterceptingInformation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        if (s.contains("9")) {
+            System.out.println("F");
+        } else {
+            System.out.println("S");
+        }
+    }
+}
