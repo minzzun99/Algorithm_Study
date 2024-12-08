12817ea3859e2aa4f7651b5b58cfd12467fd8af9
minzzun99 <skg06123@naver.com>
2024-12-08 17:27:58 +0900

    12.08_백준_Bronze5_Same Numbers

---
+++ BaekJoon/src/bronze5/Bj27324_SameNumbers.java
@@ -0,0 +1,17 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj27324_SameNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        if (s.charAt(0) == s.charAt(1)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
