0fd74c0ec496a35eecae13e962eb5df13fe74272
minzzun99 <skg06123@naver.com>
2024-12-10 11:52:43 +0900

    12.10_백준_Bronze5_Робинзон_Крузо (로빈슨 크루소)

---
+++ BaekJoon/src/bronze5/Bj27219_Робинзон_Крузо.java
@@ -0,0 +1,25 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj27219_Робинзон_Крузо {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int vCount = N / 5;
+        int iCount = N % 5;
+
+        for (int i = 0; i < vCount; i++) {
+            sb.append('V');
+        }
+
+        for (int i = 0; i < iCount; i++) {
+            sb.append('I');
+        }
+
+        System.out.println(sb);
+    }
+}
