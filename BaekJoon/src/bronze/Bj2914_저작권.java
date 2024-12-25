9a7197686ebc641132021f5bfda70f9a2582fe17
minzzun99 <skg06123@naver.com>
2024-12-25 14:57:00 +0900

    12.25_백준_Bronze3_저작권

---
+++ BaekJoon/src/bronze/Bj2914_저작권.java
@@ -0,0 +1,16 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj2914_저작권 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+        System.out.println(A * (I - 1) + 1);
+    }
+}
