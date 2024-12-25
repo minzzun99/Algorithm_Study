bd0760944fb9ac15da4724ee0d69e0fd6d5bdba6
minzzun99 <skg06123@naver.com>
2024-12-25 14:50:13 +0900

    12.25_백준_Bronze3_지능형 기차2

---
+++ BaekJoon/src/bronze/Bj2460_지능형기차2.java
@@ -0,0 +1,25 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj2460_지능형기차2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int max = 0;
+        int now = 0;
+        for (int i = 0; i < 10; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            now -= A;
+            now += B;
+            max = Math.max(max, now);
+        }
+
+        System.out.println(max);
+    }
+}
