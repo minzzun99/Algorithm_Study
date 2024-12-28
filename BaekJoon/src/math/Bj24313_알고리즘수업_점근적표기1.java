396a834229579adbcad254154ebff5059b2f1d6c
minzzun99 <skg06123@naver.com>
2024-12-28 14:24:54 +0900

    12.28_백준_Silver5_알고리즘 수업 - 점근적 표기1

---
+++ BaekJoon/src/math/Bj24313_알고리즘수업_점근적표기1.java
@@ -0,0 +1,24 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj24313_알고리즘수업_점근적표기1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+
+        if ((a1 * n0 + a0) <= c * n0 && c >= a1) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
