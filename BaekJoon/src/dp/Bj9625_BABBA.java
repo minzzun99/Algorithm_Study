1a17fddf3659305e893f2a50a59df0e3a089625c
minzzun99 <skg06123@naver.com>
2025-01-02 13:22:20 +0900

    01.02_백준_Silver5_BABBA

---
+++ BaekJoon/src/dp/Bj9625_BABBA.java
@@ -0,0 +1,24 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj9625_BABBA {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[46][2];
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        for (int i = 2; i < 46; i++) {
+            dp[i][0] = dp[i - 2][0] + dp[i - 1][0];
+            dp[i][1] = dp[i - 2][1] + dp[i - 1][1];
+        }
+
+        System.out.println(dp[N][0] + " " + dp[N][1]);
+    }
+}
