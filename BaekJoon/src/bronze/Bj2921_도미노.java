f99bff6954856ca97c4414da4a789d117ff03777
minzzun99 <skg06123@naver.com>
2025-02-25 12:03:28 +0900

    02.25_백준_Bronze3_도미노

---
+++ BaekJoon/src/bronze/Bj2921_도미노.java
@@ -0,0 +1,19 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj2921_도미노 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = i; j <= i * 2; j++) {
+                sum += j;
+            }
+        }
+        System.out.println(sum);
+    }
+}
