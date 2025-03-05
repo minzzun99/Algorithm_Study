e145bbdfdb3186348eaaf56e6bfff47c9da8221a
minzzun99 <skg06123@naver.com>
2025-03-05 13:06:41 +0900

    03.04_백준_Bronze4_임스의 메이플컵

---
+++ BaekJoon/src/bronze/Bj29790_임스의메이플컵.java
@@ -0,0 +1,25 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj29790_임스의메이플컵 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int U = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        if (N < 1000) {
+            System.out.println("Bad");
+        } else {
+            if (U >= 8000 || L >= 260) {
+                System.out.println("Very Good");
+            } else {
+                System.out.println("Good");
+            }
+        }
+    }
+}
