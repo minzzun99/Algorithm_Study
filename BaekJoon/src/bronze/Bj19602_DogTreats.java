473b8d997923262a31b4e276cc305260c5747337
minzzun99 <skg06123@naver.com>
2025-04-19 11:24:32 +0900

    04.19_백준_Bronze4_Dog Treats

---
+++ BaekJoon/src/bronze/Bj19602_DogTreats.java
@@ -0,0 +1,15 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj19602_DogTreats {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int S = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int L = Integer.parseInt(br.readLine());
+        System.out.println((S + (2 * M) + (3 * L)) >= 10 ? "happy" : "sad");
+    }
+}
