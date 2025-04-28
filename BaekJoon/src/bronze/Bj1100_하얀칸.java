3942db27412ad1283d63c389c64bd3cebabfb150
minzzun99 <skg06123@naver.com>
2025-04-28 20:21:33 +0900

    04.28_백준_Bronze2_하얀 칸

---
+++ BaekJoon/src/bronze/Bj1100_하얀칸.java
@@ -0,0 +1,21 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj1100_하얀칸 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < 8; j++) {
+                if (s.charAt(j) == 'F' && (i + j) % 2 == 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
