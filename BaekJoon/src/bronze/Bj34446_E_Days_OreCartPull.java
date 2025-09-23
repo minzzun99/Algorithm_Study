1013ef2341eb7f029771148aed7a3c61e07d1ede
minzzun99 <skg06123@naver.com>
2025-09-23 21:24:27 +0900

    09.23_백준_Bronze5_E-Days Ore Cart Pull

---
+++ BaekJoon/src/bronze/Bj34446_E_Days_OreCartPull.java
@@ -0,0 +1,15 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj34446_E_Days_OreCartPull {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
+        System.out.println(m * 2);
+    }
+}
