6472544d0916ee4c87290e0728436c846b64c4e7
minzzun99 <skg06123@naver.com>
2025-10-17 11:53:07 +0900

    10.17_백준_Bronze3_최소,최대2

---
+++ BaekJoon/src/bronze/Bj20053_최소최대2.java
@@ -0,0 +1,27 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bj20053_최소최대2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+            sb.append(arr[0]).append(" ").append(arr[N - 1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
