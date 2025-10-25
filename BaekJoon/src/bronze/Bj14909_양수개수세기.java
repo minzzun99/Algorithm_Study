9cb8f8e5ea3dba0b848b1c8152403bb3f13acc07
minzzun99 <skg06123@naver.com>
2025-10-25 15:17:37 +0900

    10.25_백준_Bronze3_양수개수세기 -> 스트림사용

--- BaekJoon/src/bronze/Bj14909_양수개수세기.java
+++ BaekJoon/src/bronze/Bj14909_양수개수세기.java
@@ -3,20 +3,16 @@ package bronze;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Bj14909_양수개수세기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int count = 0;
-        while (st.hasMoreTokens()) {
-            int a = Integer.parseInt(st.nextToken());
-            if (a > 0) {
-                count++;
-            }
-        }
+        long count = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .filter(num -> num > 0)
+                .count();
 
         System.out.println(count);
     }
