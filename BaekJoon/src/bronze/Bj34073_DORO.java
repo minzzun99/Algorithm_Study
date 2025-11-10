4dc75e484dfd05af15380e5d4da5515fffa841c0
minzzun99 <skg06123@naver.com>
2025-11-10 11:46:44 +0900

    11.10_백준_Bronze4_DORO

---
+++ BaekJoon/src/bronze/Bj34073_DORO.java
@@ -0,0 +1,16 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Bj34073_DORO {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = br.readLine().split(" ");
+        IntStream.range(0, N).forEach(i -> System.out.print(arr[i] + "DORO "));
+    }
+}
