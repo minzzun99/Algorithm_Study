81ee3480dab4dcc811de44ee1f5f02afd52bac70
minzzun99 <skg06123@naver.com>
2025-11-27 20:01:32 +0900

    11.27_백준_Bronze3_분수 좋아해?

---
+++ BaekJoon/src/bronze/Bj10474_분수좋아해.java
@@ -0,0 +1,27 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj10474_분수좋아해 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        while(true) {
+             st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if(A == 0 && B == 0) {
+                break;
+            }
+
+            sb.append(A / B).append(" ").append(A % B).append(" / ").append(B).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
