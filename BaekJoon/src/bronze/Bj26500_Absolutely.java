409c2584d23b731ef5a73629856562dde66fd3f6
minzzun99 <skg06123@naver.com>
2025-12-08 13:15:07 +0900

    12.08_백준_Bronze4_Absolutely

---
+++ BaekJoon/src/bronze/Bj26500_Absolutely.java
@@ -0,0 +1,24 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj26500_Absolutely {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            double a = Double.parseDouble(st.nextToken());
+            double b = Double.parseDouble(st.nextToken());
+            sb.append(String.format("%.1f%n", Math.abs(b - a)));
+        }
+
+        System.out.println(sb);
+    }
+}
