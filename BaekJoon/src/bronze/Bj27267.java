d18b51cc3112279300a8c081863fc8c84a9d8c85
minzzun99 <skg06123@naver.com>
2025-12-14 18:59:36 +0900

    12.14_백준_Bronze4_Сравнение комнат

---
+++ BaekJoon/src/bronze/Bj27267.java
@@ -0,0 +1,25 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj27267 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        if (a * b > c * d) {
+            System.out.println("M");
+        } else if (a * b < c * d) {
+            System.out.println("P");
+        } else {
+            System.out.println("E");
+        }
+    }
+}
