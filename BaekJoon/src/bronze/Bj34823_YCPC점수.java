7217825d9d689c210560636c706c1b0412e9f834
minzzun99 <skg06123@naver.com>
2026-02-01 17:14:04 +0900

    02.01_백준_Bronze4_YCPC 점수

---
+++ BaekJoon/src/bronze/Bj34823_YCPC점수.java
@@ -0,0 +1,18 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj34823_YCPC점수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int y = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken()) / 2;
+        int p = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.min(y, Math.min(c, p)));
+    }
+}
