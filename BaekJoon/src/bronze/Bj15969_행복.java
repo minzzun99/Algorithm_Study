abdcf39dd90136b6b4ccb8fdef8768245d0093fb
minzzun99 <skg06123@naver.com>
2026-02-07 14:34:45 +0900

    02.07_백준_Bronze3_행복

---
+++ BaekJoon/src/bronze/Bj15969_행복.java
@@ -0,0 +1,22 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bj15969_행복 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] scores = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(scores);
+        System.out.println(scores[N - 1] - scores[0]);
+    }
+}
