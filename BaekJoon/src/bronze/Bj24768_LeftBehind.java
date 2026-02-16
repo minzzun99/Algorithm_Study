5801f1364e2a66ac3f378fd0f993aa95d57aee90
minzzun99 <skg06123@naver.com>
2026-02-16 10:17:07 +0900

    02.16_백준_Bronze4_Left Beehind

---
+++ BaekJoon/src/bronze/Bj24768_LeftBehind.java
@@ -0,0 +1,37 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj24768_LeftBehind {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x == 0 && y == 0) {
+                break;
+            }
+
+            if (x + y == 13) {
+                sb.append("Never speak again.\n");
+            } else {
+                if (x < y) {
+                    sb.append("Left beehind.\n");
+                } else if (x > y) {
+                    sb.append("To the convention.\n");
+                } else {
+                    sb.append("Undecided.\n");
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
