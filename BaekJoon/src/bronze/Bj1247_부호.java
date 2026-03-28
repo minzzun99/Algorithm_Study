ea7e1aa3448fcdd4c022b3ffd33496974de69d70
minzzun99 <skg06123@naver.com>
2026-03-29 01:50:38 +0900

    03.28_Bronze3_부호

---
+++ BaekJoon/src/bronze/Bj1247_부호.java
@@ -0,0 +1,29 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Bj1247_부호 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int i = 0; i < 3; i++) {
+            BigInteger S = new BigInteger("0");
+            int N = Integer.parseInt(br.readLine());
+
+            for(int j = 0; j < N; j++) {
+                BigInteger N2 = new BigInteger(br.readLine());
+                S = S.add(N2);
+            }
+
+            if (S.compareTo(BigInteger.ZERO) < 0) {
+                System.out.println("-");
+            } else if (S.compareTo(BigInteger.ZERO) > 0) {
+                System.out.println("+");
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
