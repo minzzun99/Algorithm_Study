042408c47c7a232881b02f2e3113cf19054433f4
minzzun99 <skg06123@naver.com>
2026-04-19 20:58:15 +0900

    04.19_백준_Bronze3_골뱅이 찍기 - ㄷ

---
+++ BaekJoon/src/bronze/Bj23804_골뱅이찍기ㄷ.java
@@ -0,0 +1,26 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj23804_골뱅이찍기ㄷ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append("@".repeat(Math.max(0, 5 * N)));
+            sb.append('\n');
+        }
+        for (int i = 0; i < 3* N; i++) {
+            sb.append("@".repeat(Math.max(0, N)));
+            sb.append('\n');
+        }
+        for (int i = 0; i < N; i++) {
+            sb.append("@".repeat(Math.max(0, 5 * N)));
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
